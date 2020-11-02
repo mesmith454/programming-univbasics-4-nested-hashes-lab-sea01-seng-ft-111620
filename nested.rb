b0b66639204c27920afe4f6a9f3e33bff0063acf
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-02 18:09:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-  programmer_hash[:dennis_ritchie][:languages].to_s
+  programmer_hash[ { :dennis_ritchie => {:languages => ["C"]}.map { |k, v| [k.to]
 end
 
 def adding_matz
