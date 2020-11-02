dd99d2da28d886eea9a67b355412573bc775295e
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-02 18:20:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,9 +83,9 @@ def adding_matz
         :known_for => "Unix",
         :languages => ["C"]
       }
-    },
+    }
     
-  programmer_hash[:yukihiro_matsumoto] = 
+  programmer_hash[:yukihiro_matsumoto] => 
     {
       :known_for => "Ruby"
       :languages => ["LISP", "C"]
